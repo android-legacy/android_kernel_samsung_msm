fec7ac2afd7a748b66797ac042fab015b27bbfb2
erikcas <erikcas1972@gmail.com>
2014-02-16 21:41:01 +0100

    msm_fb: partly return to original code

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -2083,13 +2083,12 @@ static void bl_workqueue_handler(struct work_struct *work)
 #ifdef CONFIG_MACH_JENA
 		msleep(200);
 #endif
-		down(&mfd->sem);
 		mfd->bl_level = unset_bl_level;
 		pdata->set_backlight(mfd);
 		bl_level_old = unset_bl_level;
-		up(&mfd->sem);
 		bl_updated = 1;
 	}
+	up(&mfd->sem);
 }
 
 static int msm_fb_pan_display(struct fb_var_screeninfo *var,
